ce5932f57557030f5f94f96e7fccc6ebee6ff947
Kaspar Kadalipp <kaspar.kadalipp@ut.ee>
2023-01-16 15:07:13 +0200

    All king to move any distance along unblocked diagonals

--- Poddavki.py
+++ Poddavki.py
@@ -66,6 +66,7 @@ class Poddavki:
 
     def getRegularMoves(self, row, col):
         moves = []
+        directions = []
         startingLoc = (row, col)
         piece = self.board[row][col]
         match piece:
@@ -74,12 +75,20 @@ class Poddavki:
             case 'w':
                 moves = [(row - 1, col + 1), (row - 1, col - 1)]
             case 'wk' | 'bk':
-                moves = [(row + 1, col - 1), (row + 1, col + 1), (row - 1, col + 1), (row - 1, col - 1)]
+                directions = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
 
         possibleMoves = []
         for row, col in moves:
             if self.isInBounds(row, col) and self.board[row][col] == '':
                 possibleMoves.append((startingLoc, (row, col)))
+        for r, c in directions:
+            for i in range(1, 8):
+                new_row = row + i * r
+                new_col = col + i * c
+                if self.isInBounds(new_row, new_col) and self.board[new_row][new_col] == '':
+                    possibleMoves.append((startingLoc, (new_row, new_col)))
+                else:
+                    break
         return possibleMoves
 
     def printBoard(self):
@@ -102,6 +111,7 @@ class Poddavki:
         startPosition = (row, col)
         piece = board[row][col]
         locations = []
+        directions = []
         if piece in ['b', 'bk']:
             opponentPieces = ['w', 'wk']
         else:
@@ -112,15 +122,35 @@ class Poddavki:
             case 'w':
                 locations = [((row - 1, col + 1), (row - 2, col + 2)), ((row - 1, col - 1), (row - 2, col - 2))]
             case 'wk' | 'bk':
-                locations = [((row + 1, col - 1), (row + 2, col - 2)), ((row + 1, col + 1), (row + 2, col + 2)),
-                             ((row - 1, col + 1), (row - 2, col + 2)), ((row - 1, col - 1), (row - 2, col - 2))]
+                directions = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
         skipMoves = []
-        for regular_move, take_piece in locations:
-            row, col = regular_move
+
+        for enemyPosition, nextPosition in locations:
+            row, col = enemyPosition
             if self.isInBounds(row, col) and board[row][col] in opponentPieces:
-                row, col = take_piece
+                row, col = nextPosition
                 if self.isInBounds(row, col) and board[row][col] == '':
-                    skipMoves.append((startPosition, regular_move, take_piece))
+                    skipMoves.append((startPosition, enemyPosition, nextPosition))
+
+        for r, c in directions:
+            for i in range(1, 8):
+                enemyPosition = (row + i * r, col + i * c)
+                row_1, col_1 = enemyPosition
+                if self.isInBounds(row_1, col_1):
+                    if board[row_1][col_1] in opponentPieces:
+                        for j in range(i + 1, 7):
+                            nextPosition = (row + j * r, col + j * c)
+                            row_2, col_2 = nextPosition
+                            if self.isInBounds(row_2, col_2):
+                                if board[row_2][col_2] == '':
+                                    skipMoves.append((startPosition, enemyPosition, nextPosition))
+                                else:
+                                    break  # can't move past other pieces
+                            else:
+                                break  # out of bounds
+                        break  # took opponent's piece
+                else:
+                    break  # out of bounds
         return skipMoves
 
     def getSkips(self, row, col):
